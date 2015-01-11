fc15d80d7bbb155e878d2c6a1679873b8641dd1b
José Manuel Rodríguez Madrid <jmrmadrid@Joses-MacBook-Air.local>
2015-01-11 01:55:52 +0100

    first commit

---
+++ R/ejercicio6.R
@@ -0,0 +1,19 @@
+#############################################
+# José Manuel Rodríguez Madrid              #
+# Ejercicio 6: Ejercicio extra              #
+#############################################
+
+#Crea un paquete de R (con dos o tres funciones tontas, que hagan cualquier cosa). Súbelo a GitHub. Se
+#valorará que el paquete pueda ser instalado usando devtools.
+
+echo <- function(x) {
+  return(x)
+}
+
+hello <- function(x) {
+  return(paste("hello", x))
+}
+
+bye <- function(x) {
+  return(paste("bye", x))
+}
